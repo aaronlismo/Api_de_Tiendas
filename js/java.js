78e8ee85f9b4df73cec96a80f324fce7fc4f2225
2dawaa <2dawaa@informatica.edu>
2021-02-10 11:35:40 +0000

    Comit inicial

---
+++ js/java.js
@@ -0,0 +1,24 @@
+var botonXhr = document.getElementById("XHR");
+var botonFetch = document.getElementById("FETCH");
+var botonJquery = document.getElementById("JQUERY");
+
+botonFetch.addEventListener("click",mostrarTienda);
+botonXhr.addEventListener("click",mostrarTienda);
+botonJquery.addEventListener("click",mostrarTienda);
+
+function mostrarTienda()
+{
+    borrar();
+    cargandoHtml();
+}
+
+function borrar()
+{
+    
+}
+
+function cargandoHtml()
+{
+
+}
+
