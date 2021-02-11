8e72b189286dd63197100b630a682a501bff7e0a
aaronlismo <aaronlismo@hotmail.es>
2021-02-11 09:08:48 +0000

    Configurando browsersync y añadiendo gulp-jsdoc y gulp-ESlint

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,55 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
-  
-
   var gulp        = require('gulp');
   var browserSync = require('browser-sync').create();
   var sass        = require('gulp-sass');
+  const {src, task} = require('gulp');
+  const eslint = require('gulp-eslint');
+  const jsdoc = require('gulp-jsdoc3');
+ 
+    gulp.task('doc', function (cb) {
+        const config = require('./jsdoc.json');
+    gulp.src(['./src/**/*.js'], {read: false})
+        .pipe(jsdoc(config,cb));
+    });
   
   // Static Server + watching scss/html files
-  gulp.task('serve', ['sass'], function() {
+  gulp.task('serve', gulp.series['sass'], function() {
   
       browserSync.init({
-          server: "./app"
+          server: "./"
       });
   
-      gulp.watch("app/scss/*.scss", ['sass']);
-      gulp.watch("app/*.html").on('change', browserSync.reload);
+      gulp.watch("./css/*.scss", gulp.series['sass']);
+      gulp.watch("./*.html").on('change', browserSync.reload);
+      gulp.watch("./js/*.js").on('change', browserSync.reload);
   });
   
   // Compile sass into CSS & auto-inject into browsers
   gulp.task('sass', function() {
-      return gulp.src("app/scss/*.scss")
+      return gulp.src("./css/*.scss")
           .pipe(sass())
           .pipe(gulp.dest("app/css"))
           .pipe(browserSync.stream());
   });
+
+  gulp.task('sass:watch', function()
+  {
+    gulp.watch("./css/*.scss", gulp.series['sass']);
+  });
   
-  gulp.task('default', ['serve']);
+  task('default', () => {
+    return src(['./js/*.js'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
+
+
+
+  gulp.task('default', gulp.series['serve']);
