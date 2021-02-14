80fbb5a1f161c2c1ce1a0f853d9ff325d4c5a20d
aaronlismo <aaronlismo@hotmail.es>
2021-02-14 19:28:13 +0000

    Cambios echos y parte echa el 14/02/2021

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,57 @@
-  var gulp        = require('gulp');
-  var browserSync = require('browser-sync').create();
-  var sass        = require('gulp-sass');
-  const {src, task} = require('gulp');
-  const eslint = require('gulp-eslint');
-  const jsdoc = require('gulp-jsdoc3');
- 
-    gulp.task('doc', function (cb) {
-        const config = require('./jsdoc.json');
-    gulp.src(['./src/**/*.js'], {read: false})
-        .pipe(jsdoc(config,cb));
-    });
-  
-  // Static Server + watching scss/html files
-  gulp.task('serve', gulp.series['sass'], function() {
-  
-      browserSync.init({
-          server: "./"
-      });
-  
-      gulp.watch("./css/*.scss", gulp.series['sass']);
-      gulp.watch("./*.html").on('change', browserSync.reload);
-      gulp.watch("./js/*.js").on('change', browserSync.reload);
-  });
-  
-  // Compile sass into CSS & auto-inject into browsers
-  gulp.task('sass', function() {
-      return gulp.src("./css/*.scss")
-          .pipe(sass())
-          .pipe(gulp.dest("app/css"))
-          .pipe(browserSync.stream());
-  });
+'use strict';
 
-  gulp.task('sass:watch', function()
-  {
-    gulp.watch("./css/*.scss", gulp.series['sass']);
-  });
-  
-  task('default', () => {
-    return src(['./js/*.js'])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
-        .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        .pipe(eslint.failAfterError());
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+const { src, task } = require('gulp');
+const eslint = require('gulp-eslint');
+const jsdoc = require('gulp-jsdoc3');
+
+
+sass.compiler = require("node-sass");
+
+gulp.task('doc', function (cb) {
+  const config = require('./jsdoc.json');
+  gulp.src(['./*.js'], { read: false })
+    .pipe(jsdoc(config, cb));
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function () {
+  return gulp.src("./css/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch("./css/*.scss", gulp.series(['sass']));
+});
+
+gulp.task('EsLINT', () => {
+  return src(['./js/*.js'])
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError());
 });
 
 
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.series(['sass'], function () {
 
+  browserSync.init({
+    server: "./"
+  });
 
-  gulp.task('default', gulp.series['serve']);
+  gulp.watch("./css/*.scss", gulp.series(['sass']));
+  gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch("./js/*.js").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series(['serve']));
--- js/java.js
+++ js/java.js
@@ -14,11 +14,30 @@ function mostrarTienda()
 
 function borrar()
 {
-    
+    document.querySelector("main").remove();
+    document.querySelector("body").style.backgroundColor = "silver";
+
+    let simbolo = document.createElement("i");
+
+    simbolo.setAttribute("class","fas fa-donut-large");
+    document.getElementById("loading").appendChild(simbolo);
 }
 
 function cargandoHtml()
 {
+    document.querySelector("body").style.backgroundColor = "white";
+    let nuevaEmpresa = document.createElement("p");
+    nuevaEmpresa.innerText = "Nueva Empresa";
+    nuevaEmpresa.addEventListener("click",mostrarCamposNE);
+    document.getElementById("opciones").appendChild(nuevaEmpresa);
+}
 
+
+function mostrarCamposNE()
+{
+    var temp = document.getElementsByTagName("template")[0];
+    var clon = temp.content.cloneNode(true);
+    document.getElementById("NuevaEmpresa").appendChild(clon);
+    
 }
 
